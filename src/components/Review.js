5e96e3625dfe5685aa5f0359967bc90df7592b46
Bryan Kai <bryankai@Bryans-MacBook-Pro.local>
2018-06-01 08:49:28 -0700

    Changed the reviews to have the review author name displayed.

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Panel, Button } from 'react-bootstrap'
 import { withAuthentication } from "../helper/index";
+import { request } from "../helper/index";
 import '../style/Review.css'
 
 
 const Review = ({review, handleEditReviewFormShow, handleDeleteReview, authState}) => {
-  const {id, title, text, rating, snacks_id, users_id} = review
+  const {id, title, text, rating, snacks_id, users_id, first_name} = review
+
   return (
     <div>
-      {console.log(handleEditReviewFormShow)}
       <Panel>
         <Panel.Heading>
-          <Panel.Title componentClass="h3" className='review-header'>{title} by {authState.name}
+          <Panel.Title componentClass="h3" className='review-header'>{title} by {first_name}
             {authState && authState.id===users_id ?
               <span className='buttons-container'>
                 <Button bsStyle="warning"
@@ -27,6 +28,7 @@ const Review = ({review, handleEditReviewFormShow, handleDeleteReview, authState
             : null
           }
           </Panel.Title>
+
         </Panel.Heading>
         <Panel.Body>
           <div>
