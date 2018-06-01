c3f621f346ad4d4e1f2219e90a2284539e59e286
dmshikoff <dmshikoff@gmail.com>
2018-05-31 22:16:17 -0700

    completed create new user functionality and styling

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,26 +1,32 @@
 import React from "react";
-import { Button, Jumbotron } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import "../style/Banner.css";
 import { withAuthentication } from "../helper";
 
 const Banner = ({ handleShow, authState, authStatePending }) => {
   return (
-    <Jumbotron className="banner container-fluid">
-      <h1 className="banner-title">ShiKai Snacks</h1>
-
-      <p>
-        {!authState && !authStatePending? (
-          <Button
-            className="login-modal-button"
-            bsStyle="primary"
-            bsSize="large"
-            onClick={handleShow}
-          >
-            Login
-          </Button>
-        ) : null}
-      </p>
-    </Jumbotron>
+    <div className="banner container-fluid">
+      <div className="row bannerRowOne">
+        <div className="col" />
+        <div className="col">
+          <h1 className="banner-title">ShiKai Snacks</h1>
+        </div>
+        <div className="col">
+          <p>
+            {!authState && !authStatePending ? (
+              <Button
+                className="login-modal-button"
+                bsStyle="primary"
+                bsSize="large"
+                onClick={handleShow}
+              >
+                Login
+              </Button>
+            ) : null}
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
